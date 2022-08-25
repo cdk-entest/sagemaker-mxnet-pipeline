015d23b421b302bdfa35dbeec26ebd6d4efe9fe6
entest <hai@entest.io>
2022-08-25 14:11:00 +0000

    update pyproject to ignore upper case constant and docstring missing

--- test/test-create-model.py
+++ test/test-create-model.py
@@ -1,9 +1,6 @@
 import json
 import boto3
 
-def add(a, b):
-    return a+b
-
 client = boto3.client("sagemaker", region_name="us-east-1")
 
 with open("./../config.json", "r", encoding="utf-8") as file:

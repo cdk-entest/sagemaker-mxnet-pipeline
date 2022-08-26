469fa863b36d2a99edcfb65270bdefa18dca7f60
entest <hai@entest.io>
2022-08-26 13:31:36 +0000

    ssm model test

--- cdk-pipeline/bin/cdk-pipeline.ts
+++ cdk-pipeline/bin/cdk-pipeline.ts
@@ -5,13 +5,18 @@ import { PipelineStack } from "../lib/pipeline-stack";
 
 const app = new cdk.App();
 
+// below should be better
+const codeStarArn = `arn:aws:codestar-connections:${process.env.CDK_DEFAULT_REGION}:${process.env.CDK_DEFAULT_ACCOUNT}:connection/475216ac-d91d-40c7-827d-c0da1c714f10`;
+// role passed to model
+const sagemakerRole = `arn:aws:iam::${process.env.CDK_DEFAULT_REGION}:role/service-role/AmazonSageMaker-ExecutionRole-20220818T095999`;
+// sagemaker bucker
+const bucketName = `sagemaker-${process.env.CDK_DEFAULT_REGION}-${process.env.CDK_DEFAULT_ACCOUNT}`;
+
 // pipeline
 new PipelineStack(app, "PipelineStack", {
-  codeStarArn: `arn:aws:codestar-connections:${process.env.CDK_DEFAULT_REGION}:${process.env.CDK_DEFAULT_ACCOUNT}:connection/475216ac-d91d-40c7-827d-c0da1c714f10`,
-  //
-  sageMakerRole: `arn:aws:iam::${process.env.CDK_DEFAULT_REGION}:role/service-role/AmazonSageMaker-ExecutionRole-20220818T095999`,
-  // sagemaker bucket
-  bucketName: `sagemaker-${process.env.CDK_DEFAULT_REGION}-${process.env.CDK_DEFAULT_ACCOUNT}`,
+  codeStarArn: codeStarArn,
+  sageMakerRole: sagemakerRole,
+  bucketName: bucketName,
 });
 
 // mxnet endpoint
--- sagemaker/run.py
+++ sagemaker/run.py
@@ -71,22 +71,22 @@ mnist_estimator = MXNet(
     hyperparameters={"learning-rate": 0.1},
 )
 # train in sagemaker - model params will be saved to S3
-# mnist_estimator.fit(
-#     {"train": train_data_location, "test": test_data_location}
-# )
-# # mnist_estimator.deploy(
-# #     initial_instance_count=1,
-# #     instance_type="ml.m4.xlarge",
-# #     serializer=None)
-# # get the training output - model path
-# print(f"model data: {mnist_estimator.model_data}")
-# # compile the model to neo
-# update_model_data(mnist_estimator.model_data)
-# # write model name to ssm parameter 
-# ssm = boto3.client('ssm')
-# ssm.put_parameter(
-#     Name='ModelDataUrl',
-#     Value=mnist_estimator.model_data,
-#     Type="String",
-#     Overwrite=True,
-# )
+mnist_estimator.fit(
+    {"train": train_data_location, "test": test_data_location}
+)
+# mnist_estimator.deploy(
+#     initial_instance_count=1,
+#     instance_type="ml.m4.xlarge",
+#     serializer=None)
+# get the training output - model path
+print(f"model data: {mnist_estimator.model_data}")
+# compile the model to neo
+update_model_data(mnist_estimator.model_data)
+# write model name to ssm parameter 
+ssm = boto3.client('ssm')
+ssm.put_parameter(
+    Name='ModelDataUrl',
+    Value=mnist_estimator.model_data,
+    Type="String",
+    Overwrite=True,
+)
